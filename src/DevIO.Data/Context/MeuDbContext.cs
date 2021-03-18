58bb9bfe56d186ac2d8cc1c15427923d75c25874
douglas souto de souza <douglas_bufao@yahoo.com.br>
2021-03-18 16:43:20 -0300

    Criando tabelas e banco do sistema

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,37 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Definindo padrão para colunas do tipo string não mapeadas
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+            {
+                property.SetColumnType("varchar(100)");
+            };
+
+
+            //Lendo as configurações de mapeamento 
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            //SetNull nas relaçõese de deleção entre tabelas
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Fornecedor> Fornecedors { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
